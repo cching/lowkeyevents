04b2a4cdf1d19e35166117cd0e1225abcbf76f36
Chris <cching@live.com>
2017-09-26 18:42:00 -0700

    Precompiled assets for heroku

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module LK
   class Application < Rails::Application
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
